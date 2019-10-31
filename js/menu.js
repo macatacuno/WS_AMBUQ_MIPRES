2222e1c4acb0b5655b5a1f1967295661ac958a46
Roberto Parra <macatacuno@hotmail.com>
2019-10-31 10:01:31 -0500

    segunda parte del sesarrollo del modulo de Reporte de entrega Por Numero de prescripcion

--- js/menu.js
+++ js/menu.js
@@ -6,11 +6,22 @@ function cargarPanel(){
     $('#contenido_principal').load('./vistas/panel.html');
     $(".collapse-item").removeClass("active");//elimina la clase active de cualquir item que la tenga
 }
+function cargarActualizacionManual(){
+      window.open('./Reportes/getJsonAutomaticoGlobal.php', 'Actualización General');
+/*jQuery.get("./Reportes/getJsonAutomaticoGlobal.php",function(resul){
+    alert(resul);
+   })*/
+}
 
 //////////////////////////////////WSSUMINISTROAPI///////////////////////////////////////////////////////////
 function cargarEntregaXFecha (){
-
       $('#contenido_principal').load('./Reportes/GetReporteEntregaXFecha/vistaEntregaXFecha.html'); 
+      $(".collapse-item").removeClass("active");//elimina la clase active de cualquir item que la tenga
+      $("#a_entregaxfecha").addClass("active");//agrega la clase active al item seleccionado
+}
+
+function cargarEntregaXNoPrescripcion (){
+      $('#contenido_principal').load('./Reportes/GetReporteEntregaXNoPrescripcion/ReporteEntregaXNoPrescripcion.html'); 
       $(".collapse-item").removeClass("active");//elimina la clase active de cualquir item que la tenga
       $("#a_entregaxfecha").addClass("active");//agrega la clase active al item seleccionado
 }
@@ -20,12 +31,6 @@ function cargarPrescripcionPorFecha (){
       $(".collapse-item").removeClass("active");//elimina la clase active de cualquir item que la tenga
       $("#a_prescripcionporfecha").addClass("active");//agrega la clase active al item seleccionado
 }
-//////////////////////////////////WSPRESCRIPCIÓN///////////////////////////////////////////////////////////
-function cargarActualizacionManual(){
-             window.open('./Reportes/getJsonAutomaticoGlobal.php', 'Actualización General');
-      /*jQuery.get("./Reportes/getJsonAutomaticoGlobal.php",function(resul){
-           alert(resul);
-          })*/
-}
+
 
 
