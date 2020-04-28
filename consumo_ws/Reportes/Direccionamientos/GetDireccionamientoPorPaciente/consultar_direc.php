c59b0daa41647c5cdc93d2867ee0e375359301de
Roberto Parra <macatacuno@hotmail.com>
2020-04-28 12:14:11 -0500

    se agrego la funcion para cargar la informacion de la tabla desde el la base de datos

--- consumo_ws/Reportes/Direccionamientos/GetDireccionamientoPorPaciente/consultar_direc.php
+++ consumo_ws/Reportes/Direccionamientos/GetDireccionamientoPorPaciente/consultar_direc.php
@@ -7,186 +7,109 @@ ini_set('memory_limit', '-1');
 //Variable Generales
 $json = "";
 
-$periodo_inicial = $_POST['periodo_inicial'];
-$periodo_inicial_oracle = date("d/m/Y", strtotime($periodo_inicial)); //formato originar "y/m/d"
+$NoPrescripcion = $_POST['NoPrescripcion'];
+$NoIDPaciente = $_POST['NoIDPaciente'];
 
-$periodo_final = $_POST['periodo_final'];
-$periodo_final_oracle = date("d/m/Y", strtotime($periodo_final)); //formato originar "y/m/d"
+/***************************************************************************************/
+/***************************************************************************************/
+/////////////////////////////Hoja 1 Datos Generales (Inicio)///////////////////////////
+/***************************************************************************************/
+/***************************************************************************************/
+if (($NoPrescripcion != '' && $NoPrescripcion != null) || ($NoIDPaciente != '' && $NoIDPaciente != null)) {
 
-$tipo_id = $_POST['tipo'];
-$regimen = "";
-if ($tipo_id == "1") {
-  $regimen = "Cont";
-} else 
-if ($tipo_id == "2") {
-  $regimen = "Subs";
-}
-$servicio_id = 13; // Se asigna el codigo del servicio GetDirecionamiento
-
-//Se calcula el rango de los periodos
-$date1 = new DateTime($periodo_inicial);
-$date2 = new DateTime($periodo_final);
-$diff = $date1->diff($date2);
-$cant_dias = $diff->days + 1;
-$periodo_conteo = $periodo_inicial;
-
-if ($periodo_final < $periodo_inicial) {
-  echo "<script>alert('La fecha final no puede ser menor que la fecha inicial.');</script>";
-} else {
-
-
-  // Incluir la libreria PHPExcel 
-  require_once '../../../../plugins/PHPExcel/Classes/PHPExcel.php';
-  // Crea un nuevo objeto PHPExcel
-  //$objPHPExcel = new PHPExcel();
-
-  $objXLS = new PHPExcel();
-  // Establecer propiedades
-  $objXLS->getProperties()
-    ->setCreator("Cattivo")
-    ->setLastModifiedBy("Cattivo")
-    ->setTitle("Excel AMBUQ-MIPRES")
-    ->setSubject("Excel AMBUQ-MIPRES")
-    ->setDescription("Excel para visualización de reportes de MIPRES")
-    ->setKeywords("Excel AMBUQ-MIPRES")
-    ->setCategory("Excel  AMBUQ-MIPRES Prescripciones");
-
-
-
-  /***************************************************************************************/
-  /***************************************************************************************/
-  /////////////////////////////Hoja 1 Datos Generales (Inicio)///////////////////////////
-  /***************************************************************************************/
-  /***************************************************************************************/
-  $hoja = 0;
   $query = "
-SELECT 
-ID,
-IDDIRECCIONAMIENTO,
-NOPRESCRIPCION,
-DECODE(TIPOTEC, NULL,'NO EXISTE',TIPOTEC) TIPOTEC,
-DECODE(CONTEC, NULL,'NO EXISTE',CONTEC) CONTEC,
-DECODE(TIPOIDPACIENTE, NULL,'NO EXISTE',TIPOIDPACIENTE) TIPOIDPACIENTE,
-DECODE(NOIDPACIENTE, NULL,'NO EXISTE',NOIDPACIENTE) NOIDPACIENTE,
-DECODE(NOENTREGA, NULL,'NO EXISTE',NOENTREGA) NOENTREGA,
-DECODE(NOSUBENTREGA, NULL,'NO EXISTE',NOSUBENTREGA) NOSUBENTREGA,
-DECODE(TIPOIDPROV, NULL,'NO EXISTE',TIPOIDPROV) TIPOIDPROV,
-DECODE(NOIDPROV, NULL,'NO EXISTE',NOIDPROV) NOIDPROV,
-DECODE(CODMUNENT, NULL,'NO EXISTE',CODMUNENT) CODMUNENT,
-DECODE(to_char(FECMAXENT,'DD/MM/YYYY'), NULL,'NO EXISTE',to_char(FECMAXENT,'DD/MM/YYYY')) FECMAXENT,
-DECODE(CANTTOTAENTREGAR, NULL,'NO EXISTE',CANTTOTAENTREGAR) CANTTOTAENTREGAR,
-DECODE(DIRPACIENTE, NULL,'NO EXISTE',DIRPACIENTE) DIRPACIENTE,
-DECODE(CODSERTECAENTREGAR, NULL,'NO EXISTE',CODSERTECAENTREGAR) CODSERTECAENTREGAR,
-DECODE(NOIDEPS, NULL,'NO EXISTE',NOIDEPS) NOIDEPS,
-DECODE(CODEPS, NULL,'NO EXISTE',CODEPS) CODEPS,
-DECODE(to_char(FECDIRECCIONAMIENTO,'DD/MM/YYYY HH24:MI:SS'), NULL,'NO EXISTE',to_char(FECDIRECCIONAMIENTO,'DD/MM/YYYY HH24:MI:SS')) FECDIRECCIONAMIENTO,
-DECODE(ESTDIRECCIONAMIENTO, NULL,'NO EXISTE',ESTDIRECCIONAMIENTO) ESTDIRECCIONAMIENTO,
-DECODE(to_char(FECANULACION,'DD/MM/YYYY HH24:MI:SS'), NULL,'NO EXISTE',to_char(FECANULACION,'DD/MM/YYYY HH24:MI:SS')) FECANULACION,
-to_char(REPO_PERIODO,'DD/MM/YYYY') REPO_PERIODO
-FROM WEBSERV_DIRECCIONAMIENTOS 
-where  REPO_SERV_ID=" . $servicio_id . " and REPO_TIRE_ID=" . $tipo_id . " and REPO_PERIODO BETWEEN '" . $periodo_inicial_oracle . "' AND '" . $periodo_final_oracle . "'";
+  select 
+  d.ID,
+  d.IDDIRECCIONAMIENTO,
+  decode(DESC_CODSERTECAENTREGAR,null,'NO EXISTE',DESC_CODSERTECAENTREGAR)DESC_CODSERTECAENTREGAR,
+  d.NOIDPROV,
+  FECDIRECCIONAMIENTO,
+  decode(ESTDIRECCIONAMIENTO,
+         1,'Direccionado',
+         2,'Pendiente',
+         0,'Anulado',ESTDIRECCIONAMIENTO)ESTDIRECCIONAMIENTO,
+    FECANULACION,
+  pi.NOPRESCRIPCION,
+  pi.TIPOIDPACIENTE,
+  pi.NROIDPACIENTE,
+  decode(pi.TIPOTEC,'P','PROCEDIMIENTO',
+                 'M','MEDICAMENTO',
+                 'N','PRODUCTOS NUTRICIONALES',
+                 'S','SERVICIOS COMPEMENTARIOS',
+                 'D','DISPOSITIVOS MEDICOS',pi.TIPOTEC)DESC_TIPOTEC,
+  pi.CONORDEN,
+  NOENTREGA,
+  d.CODEPS
+  from WEBSERV_DIRECCIONAMIENTOS d
+  left JOIN view_webserv_pres_info_direc pi 
+  ON trim(pi.NOPRESCRIPCION)=trim(d.NOPRESCRIPCION)  and trim(pi.NROIDPACIENTE)=trim(d.NOIDPACIENTE) 
+     and pi.CONORDEN=d.CONTEC and pi.TIPOTEC=d.TIPOTEC
+  where 1=1";
+
+  if ($NoPrescripcion != '' && $NoPrescripcion != null) {
+    $query = $query . " and trim(d.NOPRESCRIPCION)='$NoPrescripcion'";
+  };
+  if ($NoIDPaciente != '' && $NoIDPaciente != null) {
+    $query = $query . " and trim(pi.NROIDPACIENTE) = '$NoIDPaciente'";
+  }
+
+  $query = $query . " order by pi.TIPOTEC,pi.CONORDEN,NOENTREGA";
   $st_tire = oci_parse($conn_oracle, $query);
   oci_execute($st_tire, OCI_DEFAULT);
 
-
-
-  $objSheet = $objXLS->createSheet();
-  $objSheet = $objXLS->setActiveSheetIndex($hoja);
-  $objXLS->getActiveSheet()->setTitle("Direccionamientos"); // AQUI AGREGO EL NOMBRE A LA HOJA
-  // Agregar Informacion
-
-  $objSheet->setCellValue('A1', 'ID');
-  $objSheet->setCellValue('B1', 'IDDIRECCIONAMIENTO');
-  $objSheet->setCellValue('C1', 'NOPRESCRIPCION');
-  $objSheet->setCellValue('D1', 'TIPOTEC');
-  $objSheet->setCellValue('E1', 'CONTEC');
-  $objSheet->setCellValue('F1', 'TIPOIDPACIENTE');
-  $objSheet->setCellValue('G1', 'NOIDPACIENTE');
-  $objSheet->setCellValue('H1', 'NOENTREGA');
-  $objSheet->setCellValue('I1', 'NOSUBENTREGA');
-  $objSheet->setCellValue('J1', 'TIPOIDPROV');
-  $objSheet->setCellValue('K1', 'NOIDPROV');
-  $objSheet->setCellValue('L1', 'CODMUNENT');
-  $objSheet->setCellValue('M1', 'FECMAXENT');
-  $objSheet->setCellValue('N1', 'CANTTOTAENTREGAR');
-  $objSheet->setCellValue('O1', 'DIRPACIENTE');
-  $objSheet->setCellValue('P1', 'CODSERTECAENTREGAR');
-  $objSheet->setCellValue('Q1', 'NOIDEPS');
-  $objSheet->setCellValue('R1', 'CODEPS');
-  $objSheet->setCellValue('S1', 'FECDIRECCIONAMIENTO');
-  $objSheet->setCellValue('T1', 'ESTDIRECCIONAMIENTO');
-  $objSheet->setCellValue('U1', 'FECANULACION');
-  $objSheet->setCellValue('V1', 'PERIODO_CONSULTADO');
-
+  $tabla = '<table class="table display AllDataTables">
+  <thead>
+      <tr>
+          <th>id</th>
+          <th>iddireccionamiento</th>
+          <th>servicio a entregar</th>
+          <th>Nit del Proveedor</th>
+          <th>Fecha de Direccionamiento</th>
+          <th>Estado del Direccionamiento</th>
+          <th>Fecha de Anulacion</th>
+          <th>Número de prescripción</th>
+          <th>Tipo de documento</th>
+          <th>Número de Documento</th>
+          <th>Tipo de tecnología</th>
+          <th>Conteo de tecnología</th>
+          <th>Número de Entrega</th>
+          <th>Codigo de la EPS</th>
+      </tr>
+  </thead>
+  
+  <tbody>';
   $i = 1;
   while (($row = oci_fetch_array($st_tire, OCI_BOTH)) != false) {
     $i = $i + 1;
-
-    $objSheet->setCellValue('A' . $i, $row["ID"]);
-    $objSheet->setCellValue('B' . $i, $row["IDDIRECCIONAMIENTO"]);
-    $objSheet->setCellValue('C' . $i, ' ' . $row["NOPRESCRIPCION"]);
-    $objSheet->setCellValue('D' . $i, $row["TIPOTEC"]);
-    $objSheet->setCellValue('E' . $i, $row["CONTEC"]);
-    $objSheet->setCellValue('F' . $i, $row["TIPOIDPACIENTE"]);
-    $objSheet->setCellValue('G' . $i, $row["NOIDPACIENTE"]);
-    $objSheet->setCellValue('H' . $i, $row["NOENTREGA"]);
-    $objSheet->setCellValue('I' . $i, $row["NOSUBENTREGA"]);
-    $objSheet->setCellValue('J' . $i, $row["TIPOIDPROV"]);
-    $objSheet->setCellValue('K' . $i, $row["NOIDPROV"]);
-    $objSheet->setCellValue('L' . $i, $row["CODMUNENT"]);
-    $objSheet->setCellValue('M' . $i, $row["FECMAXENT"]);
-    $objSheet->setCellValue('N' . $i, $row["CANTTOTAENTREGAR"]);
-    $DIRPACIENTE = utf8_encode($row["DIRPACIENTE"]);
-    $objSheet->setCellValue('O' . $i, $DIRPACIENTE);
-    $CODSERTECAENTREGAR = utf8_encode($row["CODSERTECAENTREGAR"]);
-    $objSheet->setCellValue('P' . $i, $CODSERTECAENTREGAR);
-    $objSheet->setCellValue('Q' . $i, $row["NOIDEPS"]);
-    $objSheet->setCellValue('R' . $i, $row["CODEPS"]);
-    $objSheet->setCellValue('S' . $i, $row["FECDIRECCIONAMIENTO"]);
-    $objSheet->setCellValue('T' . $i, $row["ESTDIRECCIONAMIENTO"]);
-    $objSheet->setCellValue('U' . $i, $row["FECANULACION"]);
-    $objSheet->setCellValue('V' . $i, $row["REPO_PERIODO"]);
-    /*
-    $DESC_TIPOTRANSC = utf8_encode($row["DESC_TIPOTRANSC"]);
-    $objSheet->setCellValue('AU' . $i, $DESC_TIPOTRANSC);
-*/
+    $FECDIRECCIONAMIENTO  = str_replace(',000000', '', $row['FECDIRECCIONAMIENTO']);
+    $FECANULACION  = str_replace(',000000', '', $row['FECANULACION']);
+    //No muestra si la hora es am o PM
+    
+    $tabla = $tabla . "
+      <tr>
+            <td>" . $row['ID'] . "</td>
+            <td>" . $row['IDDIRECCIONAMIENTO'] . "</td>
+            <td>" . $row['DESC_CODSERTECAENTREGAR'] . "</td>
+            <td>" . $row['NOIDPROV'] . "</td>
+            <td>" . $FECDIRECCIONAMIENTO . "</td>
+            <td>" . $row['ESTDIRECCIONAMIENTO'] . "</td>
+            <td>" . $FECANULACION . "</td>
+            <td>" . $row['NOPRESCRIPCION'] . "</td>
+            <td>" . $row['TIPOIDPACIENTE'] . "</td>
+            <td>" . $row['NROIDPACIENTE'] . "</td>
+            <td>" . $row['DESC_TIPOTEC'] . "</td>
+            <td>" . $row['CONORDEN'] . "</td>
+            <td>" . $row['NOENTREGA'] . "</td>
+            <td>" . $row['CODEPS'] . "</td>
+      </tr>
+  ";
   }
-  oci_free_statement($st_tire);
-
-  $objXLS->getActiveSheet()->getColumnDimension("A")->setAutoSize(true);
-  $objXLS->getActiveSheet()->getColumnDimension("B")->setAutoSize(true);
-  $objXLS->getActiveSheet()->getColumnDimension("C")->setAutoSize(true);
-  $objXLS->getActiveSheet()->getColumnDimension("D")->setAutoSize(true);
-  $objXLS->getActiveSheet()->getColumnDimension("E")->setAutoSize(true);
-  $objXLS->getActiveSheet()->getColumnDimension("F")->setAutoSize(true);
-  $objXLS->getActiveSheet()->getColumnDimension("G")->setAutoSize(true);
-  $objXLS->getActiveSheet()->getColumnDimension("H")->setAutoSize(true);
-  $objXLS->getActiveSheet()->getColumnDimension("I")->setAutoSize(true);
-  $objXLS->getActiveSheet()->getColumnDimension("J")->setAutoSize(true);
-  $objXLS->getActiveSheet()->getColumnDimension("K")->setAutoSize(true);
-  $objXLS->getActiveSheet()->getColumnDimension("L")->setAutoSize(true);
-  $objXLS->getActiveSheet()->getColumnDimension("M")->setAutoSize(true);
-  $objXLS->getActiveSheet()->getColumnDimension("N")->setAutoSize(true);
-  $objXLS->getActiveSheet()->getColumnDimension("O")->setAutoSize(true);
-  $objXLS->getActiveSheet()->getColumnDimension("P")->setAutoSize(true);
-  $objXLS->getActiveSheet()->getColumnDimension("Q")->setAutoSize(true);
-
-  /***************************************************************************************/
-  /***************************************************************************************/
-  /////////////////////////////Hoja 1 Datos Generales(Fin)///////////////////////////////
-  /***************************************************************************************/
-  /***************************************************************************************/
+  $tabla = $tabla . '</tbody> </table>';
+  echo $tabla;
+} else {
 }
-
-// echo "<br>query: $query<br>";
-// Establecer la hoja activa, para que cuando se abra el documento se muestre primero.
-$objXLS->setActiveSheetIndex(0);
-// Se modifican los encabezados del HTTP para indicar que se envia un archivo de Excel.
-header("Content-Type:   application/vnd.ms-excel; charset=utf-8");
-//header('Content-Type: application/vnd.openxmlformatsofficedocument.spreadsheetml.sheet');
-header("Content-Disposition: attachment;filename=direc " . $regimen . " " . $periodo_inicial . " - " . $periodo_final . ".xlsx");
-header('Cache-Control: max-age=0');
-$objWriter = PHPExcel_IOFactory::createWriter($objXLS, 'Excel2007');
-$objWriter->save('php://output');
-//header("Location:../Administrador/ReportesTecnico.php");
-exit;
+/***************************************************************************************/
+/***************************************************************************************/
+/////////////////////////////Hoja 1 Datos Generales(Fin)///////////////////////////////
+/***************************************************************************************/
+/***************************************************************************************/
--- js/DireccionamientoXpaciente.js
+++ js/DireccionamientoXpaciente.js
@@ -1,29 +1,62 @@
-buscar_direc();
+
 function buscar_direc() {
 
     //Cargar la informacion           
     val_NoPrescripcion = $('#NoPrescripcion').val();
     val_NoIDPaciente = $('#NoIDPaciente').val();
-    tabla="  <tr>        <td>22383405</td>        <td>21661791</td>        <td>[BISOPROLOL FUMARATO] 2,5mg/1U</td>        <td>816001182</td>        <td>12/03/20 03:13:00 PM</td>        <td>Anulado</td>        <td>12/03/20 03:27:00 PM</td>        <td>20190104182009821673</td>        <td>CC</td>        <td>802449</td>        <td>MEDICAMENTO</td>        <td>1</td>        <td>1</td>        <td>ESS076</td>    </tr>    <tr>        <td>22396513</td>        <td>21674427</td>        <td>[BISOPROLOL FUMARATO] 2,5mg/1U</td>        <td>816001182</td>        <td>12/03/20 04:22:00 PM</td>        <td>Anulado</td>        <td>12/03/20 04:23:00 PM</td>        <td>20190104182009821673</td>        <td>CC</td>        <td>802449</td>        <td>MEDICAMENTO</td>        <td>1</td>        <td>1</td>        <td>ESS076</td>    </tr>    <tr>        <td>22394342</td>        <td>21672325</td>        <td>[BISOPROLOL FUMARATO] 2,5mg/1U</td>        <td>816001182</td>        <td>12/03/20 04:08:00 PM</td>        <td>Anulado</td>        <td>12/03/20 04:21:00 PM</td>        <td>20190104182009821673</td>        <td>CC</td>        <td>802449</td>        <td>MEDICAMENTO</td>        <td>1</td>        <td>1</td>        <td>ESS076</td>    </tr>    <tr>        <td>21472343</td>        <td>20785434</td>        <td>[BISOPROLOL FUMARATO] 2,5mg/1U</td>        <td>816001182</td>        <td>28/02/20 11:09:00 AM</td>        <td>Anulado</td>        <td>28/02/20 11:11:00 AM</td>        <td>20190104182009821673</td>        <td>CC</td>        <td>802449</td>        <td>MEDICAMENTO</td>        <td>1</td>        <td>1</td>        <td>ESS076</td>    </tr>";
-    $("#contenido_tabla").html(tabla);
-    /*
-        $.ajax({
-            url: './Reportes/Direccionamientos/GetDireccionamientoPorPaciente/consultar_direc.php',
-            type: "POST",
-            data: {
-                NoPrescripcion: val_NoPrescripcion,
-                NoIDPaciente: val_NoIDPaciente
 
+    $.ajax({
+        url: './consumo_ws/Reportes/Direccionamientos/GetDireccionamientoPorPaciente/consultar_direc.php',
+        type: "POST",
+        data: {
+            NoPrescripcion: val_NoPrescripcion,
+            NoIDPaciente: val_NoIDPaciente
+
+        }
+    })
+        .done(function (tabla) {
+            if(tabla!=''){
+                $("#contenido_tabla").html(tabla);
+                agregar_paginacion_datatable();
+            }else{
+                alert("Debe llenar alguno de los dos campos de busqueda");
             }
-        })
-            .done(function (data) {
-                tabla = '';
-                tabla = tabla + data;
-                $("#div_TipoTec").html(tabla);
-            })
-            .fail(function (data) {
-                alert("error:" + data);
-            });
-    */
 
+        })
+        .fail(function (tabla) {
+            alert("error:" + tabla);
+        });
+
+
+}
+
+function agregar_paginacion_datatable(){
+    
+    $('.AllDataTables').DataTable({
+        language: {
+            "sProcessing": "Procesando...",
+            "sLengthMenu": "Mostrar _MENU_ registros",
+            "sZeroRecords": "No se encontraron resultados",
+            "sEmptyTable": "Ningún dato disponible en esta tabla",
+            "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix": "",
+            "sSearch": "Buscar:",
+            "sUrl": "",
+            "sInfoThousands": ",",
+            "sLoadingRecords": "Cargando...",
+            "oPaginate": {
+                "sFirst": "Primero",
+                "sLast": "Último",
+                "sNext": "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "oAria": {
+                "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+            }
+        },
+        "lengthMenu": [[5, 10, 25, -1], [5, 10, 25, "Todos"]]
+    });
 }
