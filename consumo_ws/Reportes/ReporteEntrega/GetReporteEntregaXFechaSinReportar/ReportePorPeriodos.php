a0c8938693913f7424a24e99566b0ba3292f7117
roberto parra oficina <macatacuno@hotmail.com>
2020-07-13 16:09:22 -0500

    Cambiar la lista de los reportes de entrega que no se deben de reportar

--- consumo_ws/Reportes/ReporteEntrega/GetReporteEntregaXFechaSinReportar/ReportePorPeriodos.php
+++ consumo_ws/Reportes/ReporteEntrega/GetReporteEntregaXFechaSinReportar/ReportePorPeriodos.php
@@ -59,7 +59,7 @@ if ($periodo_final < $periodo_inicial) {
   /***************************************************************************************/
   /***************************************************************************************/
   $hoja = 0;
-  $query = "SELECT 
+  /*$query = "SELECT 
   DECODE(RE.REPO_SERV_ID,NULL,'NO EXISTE',RE.REPO_SERV_ID) REPO_SERV_ID,
   DECODE(RE.REPO_TIRE_ID,NULL,'NO EXISTE',RE.REPO_TIRE_ID)REPO_TIRE_ID,
     DECODE(to_char(RE.REPO_PERIODO,'DD/MM/YYYY'), NULL,'NO EXISTE',to_char(RE.REPO_PERIODO,'DD/MM/YYYY')) REPO_PERIODO,
@@ -106,11 +106,66 @@ LEFT JOIN  view_webserv_pres_info_direc PR
      and PR.TIPOTEC=RE.TIPOTEC 
      and PR.conorden=RE.CONTEC
 LEFT JOIN  WEBSERV_REPORTE_ENTREGA ree ON 
-     ree.NOPRESCRIPCION=re.NOPRESCRIPCION and ree.FECENTREGA=re.FECENTREGA and ree.REPO_TIRE_ID=re.REPO_TIRE_ID
-where ree.IDREPORTEENTREGA<>re.IDREPORTEENTREGA 
-and re.FECREPENTREGA<ree.FECREPENTREGA AND  RE.REPO_SERV_ID=" . $servicio_id . " and RE.REPO_TIRE_ID=" . $tipo_id . " and RE.REPO_PERIODO BETWEEN '" . $periodo_inicial_oracle . "' AND '" . $periodo_final_oracle . "'"
+     ree.NOPRESCRIPCION=re.NOPRESCRIPCION and ree.FECENTREGA=re.FECENTREGA
+       and ree.REPO_TIRE_ID=re.REPO_TIRE_ID
+       ree.IDREPORTEENTREGA<>re.IDREPORTEENTREGA  and re.FECREPENTREGA<ree.FECREPENTREGA AND 
+where  RE.REPO_SERV_ID=" . $servicio_id . " and RE.REPO_TIRE_ID=" . $tipo_id . " and RE.REPO_PERIODO BETWEEN '" . $periodo_inicial_oracle . "' AND '" . $periodo_final_oracle . "'"
+."order by re.NOPRESCRIPCION, re.FECENTREGA,RE.FECREPENTREGA,REE.FECREPENTREGA";*/
+$query = "SELECT 
+DECODE(RE.REPO_SERV_ID,NULL,'NO EXISTE',RE.REPO_SERV_ID) REPO_SERV_ID,
+DECODE(RE.REPO_TIRE_ID,NULL,'NO EXISTE',RE.REPO_TIRE_ID)REPO_TIRE_ID,
+  DECODE(to_char(RE.REPO_PERIODO,'DD/MM/YYYY'), NULL,'NO EXISTE',to_char(RE.REPO_PERIODO,'DD/MM/YYYY')) REPO_PERIODO,
+DECODE(RE.ID,NULL,'NO EXISTE',RE.ID)ID,
+DECODE(RE.IDREPORTEENTREGA,null,'NO EXISTE',RE.IDREPORTEENTREGA)IDREPORTEENTREGA,
+DECODE(RE.NOPRESCRIPCION,null,'NO EXISTE',RE.NOPRESCRIPCION)NOPRESCRIPCION,
+decode(RE.TIPOTEC,
+  'P','PROCEDIMIENTO',
+  'M','MEDICAMENTO',
+  'N','PRODUCTOS NUTRICIONALES',
+  'S','SERVICIOS COMPEMENTARIOS',
+  'D','DISPOSITIVOS MEDICOS',RE.TIPOTEC)TIPOTEC,
+DECODE(RE.CONTEC,null,'NO EXISTE',RE.CONTEC)CONTEC,
+DECODE(RE.TIPOIDPACIENTE,null,'NO EXISTE',RE.TIPOIDPACIENTE)TIPOIDPACIENTE,
+DECODE(RE.NOIDPACIENTE,null,'NO EXISTE',RE.NOIDPACIENTE)NOIDPACIENTE,
+DECODE(RE.NOENTREGA,null,'NO EXISTE',RE.NOENTREGA)NOENTREGA,
+DECODE(RE.ESTADOENTREGA,null,'NO EXISTE',RE.ESTADOENTREGA)ESTADOENTREGA,
+DECODE(RE.CAUSANOENTREGA,null,'NO EXISTE',RE.CAUSANOENTREGA)CAUSANOENTREGA,
+DECODE(RE.VALORENTREGADO,null,'NO EXISTE',RE.VALORENTREGADO)VALORENTREGADO,
+DECODE(RE.CODTECENTREGADO,null,'NO EXISTE',RE.CODTECENTREGADO)CODTECENTREGADO,
+DECODE(RE.CANTTOTENTREGADA,null,'NO EXISTE',RE.CANTTOTENTREGADA)CANTTOTENTREGADA,
+DECODE(RE.NOLOTE,null,'NO EXISTE',RE.NOLOTE)NOLOTE,
+  DECODE(to_char(re.FECENTREGA,'DD/MM/YYYY'), NULL,'NO EXISTE',to_char(re.FECENTREGA,'DD/MM/YYYY')) FECENTREGA,
+ DECODE(to_char(RE.FECREPENTREGA,'DD/MM/YYYY HH24:MI:SS'), NULL,'NO EXISTE',to_char(RE.FECREPENTREGA,'DD/MM/YYYY HH24:MI:SS')) FECREPENTREGA,
+DECODE(RE.ESTREPENTREGA,null,'NO EXISTE',RE.ESTREPENTREGA)ESTREPENTREGA,
+ DECODE(to_char(RE.FECANULACION,'DD/MM/YYYY HH24:MI:SS'), NULL,'NO EXISTE',to_char(RE.FECANULACION,'DD/MM/YYYY HH24:MI:SS')) FECANULACION,
+DECODE(PP.CODDXPPAL,null,'NO EXISTE',PP.CODDXPPAL)CODDXPPAL,
+
+DECODE(D.IDDIRECCIONAMIENTO,NULL,'NO EXISTE',D.IDDIRECCIONAMIENTO) AS IDDIRECCIONAMIENTO,
+DECODE(D.NOIDPROV,NULL,'NO EXISTE',D.NOIDPROV) AS ID_PROVEEDOR,
+DECODE(PROV.NOMBRE,NULL,'NO EXISTE',PROV.NOMBRE) AS NOMBRE_PROVEEDOR,
+DECODE(PR.CODSERTECAENTREGAR,null,'NO EXISTE',PR.CODSERTECAENTREGAR)COD_SERVICIO_TECNICO,
+DECODE(PR.DESC_CODSERTECAENTREGAR,null,'NO EXISTE',PR.DESC_CODSERTECAENTREGAR)DESC_SERVICIO_TECNICO,
+
+ DECODE(to_char(REE.FECREPENTREGA,'DD/MM/YYYY HH24:MI:SS'), 
+        NULL,'NO EXISTE',to_char(REE.FECREPENTREGA,'DD/MM/YYYY HH24:MI:SS')) FECREPENTREGA_CORREGIDA,
+ DECODE(REE.ID,NULL,'NO EXISTE',REE.ID)ID_CORREGIDO,
+ DECODE(REE.IDREPORTEENTREGA,null,'NO EXISTE',REE.IDREPORTEENTREGA)IDREPORTEENTREGA_CORREGIDA
+FROM WEBSERV_REPORTE_ENTREGA re
+RIGHT JOIN WEBSERV_DIRECCIONAMIENTOS D ON D.ID=re.ID AND D.REPO_TIRE_ID=re.REPO_TIRE_ID
+LEFT JOIN WEBSERV_PRES_LIST_PROV PROV ON PROV.NOIDPROV=D.NOIDPROV
+LEFT JOIN WEBSERV_PRES_PRES PP 
+ON  TRIM(PP.NOPRESCRIPCION) =TRIM(RE.NOPRESCRIPCION)
+LEFT JOIN  view_webserv_pres_info_direc PR 
+ON TRIM(PR.NOPRESCRIPCION) =TRIM(RE.NOPRESCRIPCION)
+   and PR.TIPOTEC=RE.TIPOTEC 
+   and PR.conorden=RE.CONTEC
+LEFT JOIN  WEBSERV_REPORTE_ENTREGA ree ON 
+   ree.NOPRESCRIPCION=re.NOPRESCRIPCION and ree.FECENTREGA=re.FECENTREGA and ree.REPO_TIRE_ID=re.REPO_TIRE_ID
+   AND ree.IDREPORTEENTREGA<>re.IDREPORTEENTREGA 
+   and re.FECREPENTREGA<ree.FECREPENTREGA
+where  RE.REPO_SERV_ID=" . $servicio_id . " and RE.REPO_TIRE_ID=" . $tipo_id . " and RE.REPO_PERIODO BETWEEN '" . $periodo_inicial_oracle . "' AND '" . $periodo_final_oracle . "'"
 ."order by re.NOPRESCRIPCION, re.FECENTREGA,RE.FECREPENTREGA,REE.FECREPENTREGA";
-  $st_tire = oci_parse($conn_oracle, $query);
+$st_tire = oci_parse($conn_oracle, $query);
   oci_execute($st_tire, OCI_DEFAULT);
 
 
@@ -119,27 +174,27 @@ and re.FECREPENTREGA<ree.FECREPENTREGA AND  RE.REPO_SERV_ID=" . $servicio_id . "
   $objSheet = $objXLS->setActiveSheetIndex($hoja);
   $objXLS->getActiveSheet()->setTitle("Reporte de entrega "); // AQUI AGREGO EL NOMBRE A LA HOJA
   // Agregar Informacion
-  $objSheet->setCellValue('A1', 'ID');
-  $objSheet->setCellValue('B1', 'IDREPORTEENTREGA');
-  $objSheet->setCellValue('C1', 'NOPRESCRIPCION');
-  $objSheet->setCellValue('D1', 'TIPOTEC');
-  $objSheet->setCellValue('E1', 'CONTEC');
-  $objSheet->setCellValue('F1', 'TIPOIDPACIENTE');
-  $objSheet->setCellValue('G1', 'NOIDPACIENTE');
-  $objSheet->setCellValue('H1', 'NOENTREGA');
-  $objSheet->setCellValue('I1', 'ESTADOENTREGA');
-  $objSheet->setCellValue('J1', 'CAUSANOENTREGA');
-  $objSheet->setCellValue('K1', 'VALORENTREGADO');
-  $objSheet->setCellValue('L1', 'CODTECENTREGADO');
-  $objSheet->setCellValue('M1', 'CANTTOTENTREGADA');
-  $objSheet->setCellValue('N1', 'NOLOTE');
-  $objSheet->setCellValue('O1', 'FECENTREGA');
-  $objSheet->setCellValue('P1', 'FECREPENTREGA');
-  $objSheet->setCellValue('Q1', 'ESTREPENTREGA');
-  $objSheet->setCellValue('R1', 'FECANULACION');
-  $objSheet->setCellValue('S1', 'ID_CORREGIDO');
-  $objSheet->setCellValue('T1', 'IDREPORTEENTREGA_CORREGIDA');
-  $objSheet->setCellValue('U1', 'FECREPENTREGA_CORREGIDA');
+  $objSheet->setCellValue('A1', 'ID_NUEVO');
+  $objSheet->setCellValue('B1', 'IDREPORTEENTREGA_NUEVO');
+  $objSheet->setCellValue('C1', 'FECREPENTREGA_NUEVO');
+  $objSheet->setCellValue('D1', 'ID');
+  $objSheet->setCellValue('E1', 'IDREPORTEENTREGA');
+  $objSheet->setCellValue('F1', 'NOPRESCRIPCION');
+  $objSheet->setCellValue('G1', 'TIPOTEC');
+  $objSheet->setCellValue('H1', 'CONTEC');
+  $objSheet->setCellValue('I1', 'TIPOIDPACIENTE');
+  $objSheet->setCellValue('J1', 'NOIDPACIENTE');
+  $objSheet->setCellValue('K1', 'NOENTREGA');
+  $objSheet->setCellValue('L1', 'ESTADOENTREGA');
+  $objSheet->setCellValue('M1', 'CAUSANOENTREGA');
+  $objSheet->setCellValue('N1', 'VALORENTREGADO');
+  $objSheet->setCellValue('O1', 'CODTECENTREGADO');
+  $objSheet->setCellValue('P1', 'CANTTOTENTREGADA');
+  $objSheet->setCellValue('Q1', 'NOLOTE');
+  $objSheet->setCellValue('R1', 'FECENTREGA');
+  $objSheet->setCellValue('S1', 'FECREPENTREGA');
+  $objSheet->setCellValue('T1', 'ESTREPENTREGA');
+  $objSheet->setCellValue('U1', 'FECANULACION');
   $objSheet->setCellValue('V1', '---DATOS TECNOLOGIA ENTREGADA-->');
   $objSheet->setCellValue('W1', 'CODDXPPAL');
   $objSheet->setCellValue('X1', 'ID_PROVEEDOR');
@@ -163,27 +218,27 @@ and re.FECREPENTREGA<ree.FECREPENTREGA AND  RE.REPO_SERV_ID=" . $servicio_id . "
       }
     }
 
-    $objSheet->setCellValue('A' . $i, $row["ID"]);
-    $objSheet->setCellValue('B' . $i, $row["IDREPORTEENTREGA"]);
-    $objSheet->setCellValue('C' . $i, ' ' . $row["NOPRESCRIPCION"]);
-    $objSheet->setCellValue('D' . $i, $row["TIPOTEC"]);
-    $objSheet->setCellValue('E' . $i, $row["CONTEC"]);
-    $objSheet->setCellValue('F' . $i, $row["TIPOIDPACIENTE"]);
-    $objSheet->setCellValue('G' . $i, $row["NOIDPACIENTE"]);
-    $objSheet->setCellValue('H' . $i, $row["NOENTREGA"]);
-    $objSheet->setCellValue('I' . $i, $row["ESTADOENTREGA"]);
-    $objSheet->setCellValue('J' . $i, $row["CAUSANOENTREGA"]);
-    $objSheet->setCellValue('K' . $i, $row["VALORENTREGADO"]);
-    $objSheet->setCellValue('L' . $i, $row["CODTECENTREGADO"]);
-    $objSheet->setCellValue('M' . $i, $row["CANTTOTENTREGADA"]);
-    $objSheet->setCellValue('N' . $i, $row["NOLOTE"]);
-    $objSheet->setCellValue('O' . $i, $row["FECENTREGA"]);
-    $objSheet->setCellValue('P' . $i, $row["FECREPENTREGA"]);
-    $objSheet->setCellValue('Q' . $i, $row["ESTREPENTREGA"]);
-    $objSheet->setCellValue('R' . $i, $row["FECANULACION"]);
-    $objSheet->setCellValue('S' . $i, $row["ID_CORREGIDO"]);
-    $objSheet->setCellValue('T' . $i, $row["IDREPORTEENTREGA_CORREGIDA"]);
-    $objSheet->setCellValue('U' . $i, $row["FECREPENTREGA_CORREGIDA"]);
+    $objSheet->setCellValue('A' . $i, $row["ID_CORREGIDO"]);
+    $objSheet->setCellValue('B' . $i, $row["IDREPORTEENTREGA_CORREGIDA"]);
+    $objSheet->setCellValue('C' . $i, $row["FECREPENTREGA_CORREGIDA"]);
+    $objSheet->setCellValue('D' . $i, $row["ID"]);
+    $objSheet->setCellValue('E' . $i, $row["IDREPORTEENTREGA"]);
+    $objSheet->setCellValue('F' . $i, ' ' . $row["NOPRESCRIPCION"]);
+    $objSheet->setCellValue('G' . $i, $row["TIPOTEC"]);
+    $objSheet->setCellValue('H' . $i, $row["CONTEC"]);
+    $objSheet->setCellValue('I' . $i, $row["TIPOIDPACIENTE"]);
+    $objSheet->setCellValue('J' . $i, $row["NOIDPACIENTE"]);
+    $objSheet->setCellValue('K' . $i, $row["NOENTREGA"]);
+    $objSheet->setCellValue('L' . $i, $row["ESTADOENTREGA"]);
+    $objSheet->setCellValue('M' . $i, $row["CAUSANOENTREGA"]);
+    $objSheet->setCellValue('N' . $i, $row["VALORENTREGADO"]);
+    $objSheet->setCellValue('O' . $i, $row["CODTECENTREGADO"]);
+    $objSheet->setCellValue('P' . $i, $row["CANTTOTENTREGADA"]);
+    $objSheet->setCellValue('K' . $i, $row["NOLOTE"]);
+    $objSheet->setCellValue('R' . $i, $row["FECENTREGA"]);
+    $objSheet->setCellValue('S' . $i, $row["FECREPENTREGA"]);
+    $objSheet->setCellValue('T' . $i, $row["ESTREPENTREGA"]);
+    $objSheet->setCellValue('U' . $i, $row["FECANULACION"]);
     $objSheet->setCellValue('V' . $i, '------------>');
     $objSheet->setCellValue('W' . $i, $row["CODDXPPAL"]);
     $objSheet->setCellValue('X' . $i, $row["ID_PROVEEDOR"]);
